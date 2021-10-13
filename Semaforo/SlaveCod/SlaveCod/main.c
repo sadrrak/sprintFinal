3ab92ed3e0da6d3cfa2ed6d613eeac9994582771
sadrrak <92343454+sadrrak@users.noreply.github.com>
2021-10-12 22:54:54 -0300

    Update main.c

--- Semaforo/SlaveCod/SlaveCod/main.c
+++ Semaforo/SlaveCod/SlaveCod/main.c
@@ -6,7 +6,7 @@
  */ 
 #define F_CPU 16000000UL
 #define BAUD 9600
-#define MYUBRR F_CPU/16/BAUD-1 //Convers„o p armanezar no registrador de baud (datasheet)
+#define MYUBRR F_CPU/16/BAUD-1 //Convers√£o p armanezar no registrador de baud (datasheet)
 #include <avr/io.h>
 #include <stdio.h>
 #include <avr/interrupt.h>
@@ -21,14 +21,14 @@ uint8_t flagShot=0;
 uint32_t tempo_ms=0;
 int main(void)
 {
-    DDRB=0xff; //hablitando todos os pinos da porta B como saÌda
-	DDRD=0x81; //habilitando o pno d7 e d0 como saÌda amarelo e camera
-	DDRC=0b0000111; //c6 entrada c2..c0 saÌda
+  	DDRB=0xff; //hablitando todos os pinos da porta B como sa√≠da
+  	DDRD=0x81; //habilitando o pno d7 e d0 como sa√≠da amarelo e camera
+	DDRC=0b0000111; //c6 entrada c2..c0 sa√≠da
 	PORTB=0x00; //Iniciando desligadas ja que depende da info do mestre
 	PORTD=0x04; //amarelo desligado e d0 desligado (camera) e d2 em NAA (borda de descida para carro que passa no vermelho)
 	//PORTC=0b1000000;
 	configRegSlave();	
-    while (1) 
+    	while (1) 
     {
 		
 	}
@@ -52,13 +52,13 @@ ISR(USART0_RX_vect){
 }
 
 ISR (TIMER0_COMPA_vect){
-	tempo_ms++;//incrementando a v·riavel
+	tempo_ms++;//incrementando a v√°riavel
 	if(tempo_ms%500 == 0)// tempo_ms eh meio segundo? se sim, desliga o "shot" da camera
 		PORTD &= 0b11111110; //desligando a camera 
 }
 
 ISR (INT0_vect){	
-		cameraSemaforo(); // chamando funcao dentro da interrupcao p verificar se passou veÌculo no vermelho			
+		cameraSemaforo(); // chamando funcao dentro da interrupcao p verificar se passou ve√≠culo no vermelho			
 }
 
 void semaforoPedestre( int8_t aux){	
@@ -71,14 +71,14 @@ void semaforoPedestre( int8_t aux){
 		PORTC &= 0b1111011;		
 	}
 }
-void acionaSinal(int8_t data){	//Recebe a posiÁ„o de ESTADOS[] e realiza a operaÁ„o de ativar o buzzer ou n„o
+void acionaSinal(int8_t data){	//Recebe a posi√ß√£o de ESTADOS[] e realiza a opera√ß√£o de ativar o buzzer ou n√£o
 	if( data== 7 || data== 8)
-		PORTC |= 0b0000001; //aciona C0 como saÌda de sinal
+		PORTC |= 0b0000001; //aciona C0 como sa√≠da de sinal
 	else
 		PORTC &= 0b1111110 ;	//desativa C0	
 }
 
-void cameraSemaforo(){ //Camera que capta se algum veÌculo passou no vermelho
+void cameraSemaforo(){ //Camera que capta se algum ve√≠culo passou no vermelho
 		if(PORTB==0b011110000 || PORTB==0b001110000 || PORTB==0b000110000 || PORTB==0b000010000)
 			PORTD |= 0b00000001;	//aciona camera	
 			
@@ -87,22 +87,22 @@ void cameraSemaforo(){ //Camera que capta se algum veÌculo passou no vermelho
 
 void configRegSlave(){
 	//Para INT0
-	EICRA= 0b00000010;	//interrupÁ„o externa INT0 na borda de descida
-	EIMSK= 0b00000001; //habilitaÁ„o das interrupÁ„o externa INT0
+	EICRA= 0b00000010;	//interrup√ß√£o externa INT0 na borda de descida
+	EIMSK= 0b00000001; //habilita√ß√£o das interrup√ß√£o externa INT0
 	
 	//TIMER0
 	TCCR0A= 0b00000010;
 	TCCR0B= 0b00000011; //ligando TC0 com prescaler= 64
 	OCR0A= 249; //define um limite de contagem para TC0
-	TIMSK0= 0b00000010; //habilitando a interrupÁ„o na igualdade de OCR0A. A interrrupÁ„o chega em 1ms (64*(249+1)/16M Hz)
+	TIMSK0= 0b00000010; //habilitando a interrup√ß√£o na igualdade de OCR0A. A interrrup√ß√£o chega em 1ms (64*(249+1)/16M Hz)
 	
 	//Usart Config
-	UBRR0H= (unsigned char)(MYUBRR>>8); //ajusta taxa de transmiss„o, parte alta
-	UBRR0L= (unsigned char)MYUBRR; //ajusta a taxa de transmiss„o, parte baixa
-	UCSR0B= (1<<RXCIE0)|(1<<RXEN0)|(1<<TXEN0); //habilitando interrupÁ„o do receptor, habilita transmissor e receptor
+	UBRR0H= (unsigned char)(MYUBRR>>8); //ajusta taxa de transmiss√£o, parte alta
+	UBRR0L= (unsigned char)MYUBRR; //ajusta a taxa de transmiss√£o, parte baixa
+	UCSR0B= (1<<RXCIE0)|(1<<RXEN0)|(1<<TXEN0); //habilitando interrup√ß√£o do receptor, habilita transmissor e receptor
 	UCSR0C= (3<<UCSZ00); //ajuste de formato do frame 8 bits de dados, 1 de parada, no parity
 	
-	sei();//habilitando as interrupÁıes
+	sei();//habilitando as interrup√ß√µes
 }
 	
 	
